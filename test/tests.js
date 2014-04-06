208df1a7c664dd4096b0434a4e62c27cb5b9ecfe
Max Nachlinger <max.nachlinger@gmail.com>
2014-04-06 10:04:27 -0700

    Added a switch-language test

--- test/tests.js
+++ test/tests.js
@@ -7,7 +7,8 @@ var languageFns;
 
 test('Setup', function(t) {
 	mfLanguage({
-		'test': fs.readFileSync(path.join(__dirname, 'messages.po'))
+		'test0': fs.readFileSync(path.join(__dirname, 'test0Messages.po')),
+		'test1': fs.readFileSync(path.join(__dirname, 'test1Messages.po'))
 	}, function(err, res) {
 		t.notOk(err, "Did not error on initialize, received: " + util.inspect(err, {depth: null}));
 		t.ok(res, "A result was returned from initialize, received: " + util.inspect(res, {depth: null}));
@@ -17,26 +18,34 @@ test('Setup', function(t) {
 });
 
 test('Simple strings work', function(t) {
-	var fns = languageFns('test'); // use languages/test/messages.po
-	t.equal(fns.getText("Simple string"), "Simple string translated");
+	var fns = languageFns('test0');
+	t.equal(fns.getText("test0 - Simple string"), "test0 - Simple string translated");
 	t.end();
 });
 
 test('sprintf place-holder strings work', function(t) {
-	var fns = languageFns('test'); // use languages/test/messages.po
-	t.equal(fns.getText("1 string place-holder: %s", "test-string"), "1 string place-holder: test-string translated");
-	t.equal(fns.getText("1 string, 1 int place-holder: %s, %d", "test-string", 30), "1 string, 1 int place-holder: test-string, 30 translated");
+	var fns = languageFns('test0');
+	t.equal(fns.getText("test0 - 1 string place-holder: %s", "test-string"), "test0 - 1 string place-holder: test-string translated");
+	t.equal(fns.getText("test0 - 1 string, 1 int place-holder: %s, %d", "test-string", 30), "test0 - 1 string, 1 int place-holder: test-string, 30 translated");
 	t.end();
 });
 
 test('plural place-holder strings work', function(t) {
-	var fns = languageFns('test'); // use languages/test/messages.po
-	t.equal(fns.getPluralText("Simple string singular", 2), "Simple string translated plural");
+	var fns = languageFns('test0');
+	t.equal(fns.getPluralText("test0 - Simple string singular", 2), "test0 - Simple string translated plural");
 	t.end();
 });
 
 test('sprintf plural place-holder strings work', function(t) {
-	var fns = languageFns('test'); // use languages/test/messages.po
-	t.equal(fns.getPluralText("Simple string singular int place-holder: %d", 2, 20), "Simple string translated plural int place-holder: 20");
+	var fns = languageFns('test0');
+	t.equal(fns.getPluralText("test0 - Simple string singular int place-holder: %d", 2, 20), "test0 - Simple string translated plural int place-holder: 20");
+	t.end();
+});
+
+test('switching language works', function(t) {
+	var fns0 = languageFns('test0');
+	t.equal(fns0.getText("test0 - Simple string"), "test0 - Simple string translated");
+	var fns1 = languageFns('test1');
+	t.equal(fns1.getText("test1 - Simple string"), "test1 - Simple string translated");
 	t.end();
 });
